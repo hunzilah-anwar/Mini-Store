c630d07d5085f65e3fb71fb249d8f9c7a68770be
hunzilah-anwar <hunzilahanwar786@gmail.com>
2025-06-14 01:11:20 +0500

    ok

--- JS/script.js
+++ JS/script.js
@@ -4,5 +4,5 @@
             sidemenu.style.right = '0';
         }
         function closeMenu(){
-            sidemenu.style.right = '-200px'
+            sidemenu.style.right = '-300px'
         }
